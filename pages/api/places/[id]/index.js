de3c3cccd5d68ee3dba3b0ff0877133945caff7a
Moritz Hoyer <contact@moritzhoyer.de>
2024-09-23 13:22:27 +0200

    implement delete functionality

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,21 +1,21 @@
-import dbConnect from '../../../../db/connect';
-import Place from '../../../../db/models/Places';
+import dbConnect from "../../../../db/connect";
+import Place from "../../../../db/models/Places";
 
 export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
 
-  if (request.method === 'GET') {
+  if (request.method === "GET") {
     const place = await Place.findById(id);
 
     if (!place) {
-      return response.status(404).json({ status: 'Not Found' });
+      return response.status(404).json({ status: "Not Found" });
     }
 
     response.status(200).json({ place: place });
   }
   //Handle PATCH request to update a place
-  if (request.method === 'PATCH') {
+  if (request.method === "PATCH") {
     const { id } = request.query; // Retrieve id from the query
 
     try {
@@ -26,18 +26,38 @@ export default async function handler(request, response) {
       );
 
       if (!updatedPlace) {
-        return response.status(404).json({ status: 'Place not found' });
+        return response.status(404).json({ status: "Place not found" });
       }
-      console.log('response', response.status(200).json(updatedPlace));
+      console.log("response", response.status(200).json(updatedPlace));
 
       return response.status(200).json(updatedPlace); // Respond with the updated place
     } catch (error) {
       return response
         .status(400)
-        .json({ message: 'Failed to update place', error: error.message });
+        .json({ message: "Failed to update place", error: error.message });
     }
   }
+  if (request.method === "DELETE") {
+    const { id } = request.query;
+
+    try {
+      const deteledPlace = await Place.findByIdAndDelete(id);
+      if (!deteledPlace) {
+        return response.status(404).json({ status: "Place not found" });
+      }
+      return response
+        .status(200)
+        .json({ message: "Place successfully deleted" });
+    } catch (error) {
+      return response
+        .status(400)
+        .json({ message: "Failed to delete Place", error: error.message });
+    }
+  }
+  // If the request method is not supported
+  return response.status(405).json({ message: "Method not allowed" });
 }
+
 // import { db_places } from "../../../../lib/db_places";
 // import { db_comments } from "../../../../lib/db_comments";
 
